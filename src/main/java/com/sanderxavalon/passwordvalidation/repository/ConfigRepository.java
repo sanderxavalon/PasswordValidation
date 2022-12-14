a691c26e1805c4a6897dc123ef5752a883f28969
Sander.Chen <Sander.Chen@innovasolutions.com>
2022-12-14 19:07:18 +0800

    minor fix

--- src/main/java/com/sanderxavalon/passwordvalidation/repository/ConfigRepository.java
+++ src/main/java/com/sanderxavalon/passwordvalidation/repository/ConfigRepository.java
@@ -5,9 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ConfigRepository extends JpaRepository<Config, Integer>  {
-
-    @Query(value = "select * from config where config_key = :configKey",nativeQuery = true)
-    Config findOneByConfigKey(@Param("configKey") String configKey);
-
-}
+public interface ConfigRepository extends JpaRepository<Config, Integer>  { }
